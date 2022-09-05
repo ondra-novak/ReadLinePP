c23d06927b1d12e41536419c63b082f1e8cd92e9
Ondřej Novák <novak@cooberfund.com>
2022-09-05 10:42:11 +0200

    extend demo by testing matches from regex

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,27 @@
 
+#include <fstream>
 #include <iostream>
 
 #include "readlinepp.h"
+
+ReadLine::GenFn extractFile(std::string path) {
+    return [path](const char *word, std::size_t word_len, const std::cmatch &m, const ReadLine::ProposalCallback &cb) {
+        std::string fullpath = path+"/"+std::string(m[1].first,m[1].second);
+        std::ifstream x(fullpath);
+        if (!x) return;
+        std::string ln;
+        while (!!std::getline(x,ln)) {
+            auto n = ln.find_first_not_of(" \t");
+            if (n != ln.npos) {
+                ln.erase(0,n);
+                if (ln.compare(0,word_len,word) == 0) {
+                    cb(ln);
+                }
+            }
+        }
+    };
+}
+
 
 int main(int argv, char **argc) {
 
@@ -15,6 +35,7 @@ int main(int argv, char **argc) {
         {"hi ",{"ondra","franta"}},
         {"file ",ReadLine::fileLookup(".")},
         {"csource ",ReadLine::fileLookup(".",".*\\.c|.*\\.cpp|.*\\.h|.*\\/")},
+        {"csource ([^ ]+) ",extractFile(".")},
     });
     rl.setAppName("rldemo");
     std::string line;
